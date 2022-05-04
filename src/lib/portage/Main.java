f1aeff3bbda9a9465055e6e6fafa53c2c394c630
a-weeb-programmer <austinortega@outlook.com>
2022-05-04 12:10:09 -0600

    initial commit

---
+++ src/lib/portage/Main.java
@@ -0,0 +1,8 @@
+package lib.portage;
+
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("This will eventually be a package manager, but for now, it's a hello world program\n"
+				+ "hello world");
+	}
+}
