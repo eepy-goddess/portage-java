6cd7c74ad1bc0b604ea0be059b4c3ccf149859dc
a weeb programmer <65640526+a-weeb-programmer@users.noreply.github.com>
2022-05-26 15:47:06 -0600

    Cleaned up more code

--- src/main/java/lib/portage/Main.java
+++ src/main/java/lib/portage/Main.java
@@ -3,18 +3,14 @@ package lib.portage;
 import java.util.ArrayList;
 import java.util.List;
 
-import lib.portage.configuration.MakeconfReader;
-import lib.portage.enums.EmergeFlags;
 import lib.portage.interfaces.FlagFeature;
 import lib.portage.package_management.Emerge;
-import lib.portage.package_management.EmergePackage;
 
 public class Main {
 	//F
-	public static List<FlagFeature> flags = new ArrayList<FlagFeature>();
+	public static List<FlagFeature> flags = new ArrayList<>();
 
-	public static void main(String[] args) throws Exception {
-		MakeconfReader conf = new MakeconfReader("./etc/portage/make.conf");
+	public static void main(String[] args) {
 		Emerge.command(args);
 		
 		
@@ -23,8 +19,6 @@ public class Main {
 		for(FlagFeature o : flags) {
 			if(o.flagId() == id) {
 				return o;
-			}else {
-				continue;
 			}
 		}
 		return null;
